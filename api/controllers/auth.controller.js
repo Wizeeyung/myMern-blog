910176cb3c98c2e61434baa8f97efca33b0cf4a2
wizeeyung <ogunbanwosamson@gmail.com>
2024-03-15 15:29:00 +0000

    creating signin api rout

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.models.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) =>{
   //get the body from req.body by destructuring
@@ -32,4 +33,46 @@ export const signup = async (req, res, next) =>{
   }
 
   
+};
+
+
+
+export const signin = async (req, res, next) => {
+
+  const {email, password} = req.body;
+  if(!email || !password){
+    return next(errorHandler(404, 'All fields are required'))
+  }
+
+  try{
+    //remember to always use await as you're trying to get data from the database
+    const validUser = await User.findOne({email});
+    if(!validUser){
+      return next(errorHandler(404, 'user not found'));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    //always remember to add return statement to the if statement so it doesnt go to the next line
+    if(!validPassword){
+      return next(errorHandler(400, 'Invalid password'))
+    }
+
+    //to get the token we use the json web token and also use a secret key
+    const token = jwt.sign(
+      {id: validUser._id},
+      process.env.JWT_SECRETKEY, 
+    );
+
+    //destructure the valid user and dont forget to give the password a variable and also use ._doc to get only the metadata available in the validUser
+    const {password: pass, ...rest} = validUser._doc
+
+    res.status(200).cookie('access_token', token,{httpOnly: true}).json(rest);
+  
+
+  }catch (error){
+    next(error)
+
+  }
+
 }
