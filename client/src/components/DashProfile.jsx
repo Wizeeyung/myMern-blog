425fa161d84d0b4a4e19e73a0740009c38fa0940
wizeeyung <ogunbanwosamson@gmail.com>
2024-03-26 01:06:57 +0000

    complete profile page UI

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,15 +1,20 @@
+import { useSelector } from "react-redux";
+
 
 const DashProfile = () => {
+
+  const {currentUser} = useSelector((state)=> state.user)
   return (
     <div className="dash-profile">
-      <div className="profile-pic-container">
-
-      </div>
+      <h1>Profile</h1>
       <form className="profile-form">
-        <input type="text" placeholder="username"/>
-        <input type="email" placeholder="email"/>
-        <input type="password" placeholder="password"/>
-        <button className="profile-form-btn">Update</button>
+        <div className="profile-pic-container">
+          <img src={currentUser.profilePicture} />
+        </div>
+        <input type="text" id="username" placeholder="username" defaultValue={currentUser.username}/>
+        <input type="email" id="email" placeholder="email" defaultValue={currentUser.email}/>
+        <input type="password" id="password" placeholder="password"/>
+        <button className="profile-form-btn" type="submit">Update</button>
       </form>
       <div className="profile-form-p">
         <p>Delete Account</p>
