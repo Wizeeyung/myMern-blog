26b62cf979525cd8b925f66e42eeb69cc50c1264
wizeeyung <ogunbanwosamson@gmail.com>
2024-04-02 16:06:00 +0100

    add delete user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import { test, updateUser } from '../controllers/user.controller.js';
 import { veriifyToken } from '../utils/verifyUser.js';
+import { deleteUser } from '../controllers/user.controller.js';
 
 
 const router = express.Router();
 
 router.get('/test', test)
+//we need to verify that only the person logged in can update the profile and also delete it
 router.put('/update/:userId',veriifyToken, updateUser);
+router.delete('/delete/:userId', veriifyToken, deleteUser);
+
 
 export default router;
