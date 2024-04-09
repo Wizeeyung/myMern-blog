44bbc3c8cb65d12a14295d71e7a441a4928f5cf9
wizeeyung <ogunbanwosamson@gmail.com>
2024-04-10 00:36:41 +0100

    get post api complete functionality

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { veriifyToken } from '../utils/verifyUser.js';
-import { createPost } from '../controllers/post.controller.js';
+import { createPost , getPosts} from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.post('/create', veriifyToken, createPost);
+router.get('/getpost', getPosts);
 
 export default router;
