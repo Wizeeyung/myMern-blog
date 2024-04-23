8d27240b517af8225208e6479744ecccb45c0738
wizeeyung <ogunbanwosamson@gmail.com>
2024-04-24 00:49:58 +0100

    cta on post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -70,7 +70,7 @@ const PostPage = () => {
         <div className="cta-left">
           <div className="cta-left-content">
           <h1>Want to know more about how amazing <br/> my projects are?</h1>
-          <span>Check out my projects by clicking the button below</span>
+          <span>Check out my projects by clicking on the button below</span>
           </div>
           {/* rel='noopener noreferrer'  attribute is added for security reasons to prevent a potential security vulnerability called "tabnabbing". */}
           <button><Link to='https://github.com/Wizeeyung?tab=repositories' target='_blank' rel='noopener noreferrer'>My Projects</Link></button>
